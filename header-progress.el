35152f10584eeabe7f98bbb16dd4cb89f5e0f367
Abhinav Tushar <abhinav@lepisma.xyz>
2024-10-26 16:02:35 +0530

    Add indeterminate bar

--- header-progress.el
+++ header-progress.el
@@ -57,6 +57,12 @@ This is used for restoring once the progress bar is finished.")
   "Variable that can be set by programs to update header progress
 bar. It is a float between 0 to 1.")
 
+(defvar-local hp-gradient-patch nil
+  "Patch of colored string to be used for indeterminate bar.")
+
+(defvar-local hp-gradient-offset 0
+  "Offset to be used for animating the gradient patch.")
+
 (defun hp--build-bar (ratio)
   "Return a header bar string to show the given RATIO."
   (let* ((total (window-width))
@@ -69,12 +75,49 @@ bar. It is a float between 0 to 1.")
   (hp--build-bar (/ (float (line-number-at-pos)) (count-lines (point-min) (point-max)))))
 
 (defun hp-buffer-progress-bar-wide ()
-  "Similar to `hp-buffer-progress-bar' but ignores any narrowing in effect."
+  "Similar to `hp-buffer-progress-bar' but ignores any narrowing in
+effect."
   (hp--build-bar (/ (float (line-number-at-pos nil t)) (count-lines 1 (+ 1 (buffer-size))))))
 
 (defun hp-progress-bar ()
   "Build a bar to show progress as indicated in the variable HEADER-PROGRESS-RATIO."
   (hp--build-bar hp-ratio))
+
+(defun generate-wrapping-gradient-patch (color1 color2 cycle-length)
+  (let* ((r1 (nth 0 (color-values color1)))
+         (g1 (nth 1 (color-values color1)))
+         (b1 (nth 2 (color-values color1)))
+         (r2 (nth 0 (color-values color2)))
+         (g2 (nth 1 (color-values color2)))
+         (b2 (nth 2 (color-values color2)))
+         (vector (make-vector cycle-length nil)))
+    (dotimes (j cycle-length)
+      (let* ((factor (/ (+ 1 (sin (* 2 pi (/ j (float cycle-length))))) 2))
+             (r (+ r1 (* factor (- r2 r1))))
+             (g (+ g1 (* factor (- g2 g1))))
+             (b (+ b1 (* factor (- b2 b1))))
+             (color (format "#%02x%02x%02x" (/ r 256) (/ g 256) (/ b 256))))
+        (aset vector j (propertize "▀" 'face `(:foreground ,color)))))
+    (apply 'concat (append vector nil))))
+
+(defun hp--repeat-with-offset (patch total-length offset)
+  "Repeat string PATCH across TOTAL-LENGTH with given OFFSET.
+The repeated pattern starts at OFFSET within the repeated sequence."
+  ;; TODO: This is a buggy implementation of what the function says it does. But
+  ;; for our purpose this works since header-line display does truncation
+  ;; automatically.
+  (let* ((patch-len (length patch))
+         (pre-patch (substring patch offset))
+         (remaining-length (- total-length (length pre-patch)))
+         (repeat-count (+ 2 (/ remaining-length patch-len)))
+         (tiles (apply 'concat (make-list repeat-count patch))))
+    (concat pre-patch tiles)))
+
+(defun hp-indeterminate-bar ()
+  (let ((width (window-width))
+        (patch-length (length hp-gradient-patch)))
+    (setq hp-gradient-offset (mod (+ 1 hp-gradient-offset) patch-length))
+    (hp--repeat-with-offset hp-gradient-patch width hp-gradient-offset)))
 
 (defun hp-bar-start (bar-function)
   "Save the original header bar and initialize the header bar with
